50a4983e45113b5706b9fed023b0507e5a36f244
Pedro Rambo <henrique.pr92@gmail.com>
2020-03-25 11:28:17 -0300

    Add ongs controller

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,8 @@
 const express = require('express');
 const routes = express.Router();
-const crypto = require('crypto');
-const connection = require('./database/connection');
+const OngsController = require('./controllers/OngsController');
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-    return response.json(ongs);
-})
-
-routes.post('/ongs', async (request, response) => {
-    const {name, email, whatsapp, city, uf} = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-        id
-    })
-
-    return response.json({id});
-})
+routes.get('/ongs', OngsController.index);
+routes.post('/ongs', OngsController.create);
 
 module.exports = routes;
