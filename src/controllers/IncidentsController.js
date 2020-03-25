7680483bc48724c85983b9a19e166ac5efd5da32
Pedro Rambo <henrique.pr92@gmail.com>
2020-03-25 13:13:58 -0300

    Add incident controller

---
+++ src/controllers/IncidentsController.js
@@ -0,0 +1,51 @@
+const connection = require('../database/connection');
+
+async function index(request, response){
+    const {authorization} = request.headers;
+
+    if(authorization){
+        const incidents = await connection('incidents')
+            .where('ong_id', authorization)
+            .select('*');
+        return response.json(incidents);
+    }
+
+    const incidents = await connection('incidents').select('*');
+    return response.json(incidents);
+}
+
+async function create(request, response){
+    const {title, description, value} = request.body;
+    const {authorization} = request.headers;
+
+    const [id] = await connection('incidents').insert({
+        title,
+        description,
+        value,
+        ong_id: authorization
+    })
+
+    return response.json({id});
+}
+
+async function remove(request, response){
+    const {id} = request.params;
+    const {authorization} = request.headers;
+
+    const {ong_id} = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+    if(ong_id != authorization){
+        return response.status(401).json({error: "Operation not permitted"})
+    }
+
+    await connection('incidents')
+        .where('id', id)
+        .delete();
+
+    return response.status(204).send();
+}
+
+module.exports = {index, create, remove};
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 const OngsController = require('./controllers/OngsController');
+const IncidentsController = require('./controllers/IncidentsController');
 
 routes.get('/ongs', OngsController.index);
 routes.post('/ongs', OngsController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.remove);
 
 module.exports = routes;
