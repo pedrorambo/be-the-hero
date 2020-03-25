0734fa9fe26ebefc981659f12d05f8fb8b89df0f
Pedro Rambo <henrique.pr92@gmail.com>
2020-03-25 11:09:22 -0300

    Add get ongs

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ const routes = express.Router();
 const crypto = require('crypto');
 const connection = require('./database/connection');
 
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+    return response.json(ongs);
+})
+
 routes.post('/ongs', async (request, response) => {
     const {name, email, whatsapp, city, uf} = request.body;
     const id = crypto.randomBytes(4).toString('HEX');
