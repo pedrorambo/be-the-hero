a41bcc76c5938383db95dc6d9a903083769669ff
Pedro Rambo <henrique.pr92@gmail.com>
2020-04-02 15:15:12 -0300

    Add new incicent connection with API

--- frontend/src/pages/new-incident/NewIncident.js
+++ frontend/src/pages/new-incident/NewIncident.js
@@ -2,8 +2,9 @@ import React from 'react';
 import logo from "../../assets/logo.svg";
 import {Link} from "react-router-dom";
 import {FiChevronsLeft} from 'react-icons/fi';
+import api from "../../services/api";
 
-export default class NewIncident extends React.Component{
+export default class NewIncident extends React.Component {
 
     constructor(props) {
         super(props);
@@ -15,12 +16,32 @@ export default class NewIncident extends React.Component{
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e){
+    handleChange(e) {
         const key = e.target.name;
         const value = e.target.value;
-        this.setState({[key]: value });
+        this.setState({[key]: value});
+    }
+
+    // Submit the form
+    async handleSubmit(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('incidents', this.state,
+                {
+                    headers: {
+                        Authorization: localStorage.getItem('ongId'),
+                    }
+                });
+            alert("Caso cadastrado com sucesso!");
+            this.props.history.push('/profile');
+        } catch (e) {
+            console.log(e);
+            alert("Erro ao cadastrar caso, tente novamente!");
+        }
     }
 
     render() {
@@ -32,7 +53,7 @@ export default class NewIncident extends React.Component{
                     <div className="row">
 
                         {/*DESCRIPTION*/}
-                        <div className="col-lg-2"></div>
+                        <div className="col-lg-2"/>
 
                         <div className="col-lg-4 my-auto">
                             <img src={logo} alt="Be the hero logo"/>
@@ -48,22 +69,27 @@ export default class NewIncident extends React.Component{
 
                         {/*INCIDENT FORM*/}
                         <div className="col-lg-4 my-auto">
-                            <form>
+                            <form onSubmit={this.handleSubmit}>
                                 <div className="form-group mb-1">
-                                    <input name={'title'} value={title} onChange={this.handleChange} className={'form-control'} placeholder={'Título do caso'}/>
+                                    <input name={'title'} value={title} onChange={this.handleChange}
+                                           className={'form-control'} placeholder={'Título do caso'}/>
                                 </div>
 
                                 <div className="form-group mb-1">
-                                    <textarea name={'description'} value={description} onChange={this.handleChange} rows={5} className={'form-control'} placeholder={'Descrição'}/>
+                                    <textarea name={'description'} value={description} onChange={this.handleChange}
+                                              rows={5} className={'form-control'} placeholder={'Descrição'}/>
                                 </div>
 
                                 <div className="form-group mb-2">
-                                    <input name={'value'} value={value} onChange={this.handleChange} className={'form-control'} placeholder={'Valor em reais'}/>
+                                    <input name={'value'} value={value} onChange={this.handleChange}
+                                           className={'form-control'} placeholder={'Valor em reais'}/>
                                 </div>
 
                                 <div className="form-row">
                                     <div className="form-group col-lg-4 mb-0">
-                                        <input type="submit" className={'btn btn-outline-secondary btn-block'} value={'Cancelar'}/>
+                                        <Link to={'/profile'}>
+                                            <button className={'btn btn-outline-secondary btn-block'}>Cancelar</button>
+                                        </Link>
                                     </div>
                                     <div className="form-group col-lg-8 mb-0">
                                         <input type="submit" className={'btn btn-hero btn-block'} value={'Cadastrar'}/>
@@ -72,7 +98,7 @@ export default class NewIncident extends React.Component{
                             </form>
                         </div>
 
-                        <div className="col-lg-2"></div>
+                        <div className="col-lg-2"/>
                     </div>
                 </div>
             </div>
