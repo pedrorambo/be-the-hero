807dc83c967d8ef8d73b3ad5fc2f178bc88cd653
Pedro Rambo <henrique.pr92@gmail.com>
2020-04-02 14:17:08 -0300

    Add guest page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,16 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Register from "./pages/register/Register";
 import Profile from "./pages/profile/Profile";
 import NewIncident from "./pages/new-incident/NewIncident";
+import Guest from "./pages/guest/Guest";
+import Incidents from "./pages/incidents/Incidents";
 
 function App() {
   return (
       <Router>
           <Switch>
-              <Route path={'/'} exact component={Logon}/>
+              <Route path={'/'} exact component={Guest}/>
+              <Route path={'/logon'} component={Logon}/>
+              <Route path={'/incidents'} component={Incidents}/>
               <Route path={'/register'} component={Register}/>
               <Route path={'/profile'} component={Profile}/>
               <Route path={'/incidents/new'} component={NewIncident}/>
